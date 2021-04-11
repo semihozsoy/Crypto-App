764b19079ce3f7ef64ae86165d0bd03076ae339a
semih <semih__ozsoy@hotmail.com>
2021-04-11 19:21:35 +0300

    writing model

---
+++ CryptoApp/Model/CryptoCurrency.swift
@@ -0,0 +1,14 @@
+//
+//  CryptoCurrency.swift
+//  CryptoApp
+//
+//  Created by Semih Özsoy on 11.04.2021.
+//
+
+import Foundation
+
+
+struct CryptoCurrency: Decodable {
+    let currency: String
+    let prices: String
+}
