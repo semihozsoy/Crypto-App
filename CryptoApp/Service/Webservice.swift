09cdb0f88e7888dc63e628f7b1c9c910cb13c413
semih <semih__ozsoy@hotmail.com>
2021-04-12 13:31:13 +0300

    fetching data

---
+++ CryptoApp/Service/Webservice.swift
@@ -0,0 +1,35 @@
+//
+//  Webservice.swift
+//  CryptoApp
+//
+//  Created by Semih Özsoy on 11.04.2021.
+//
+
+import Foundation
+
+
+class Webservice {
+    
+    // CryptoCurrency'den gelen data JSON datası yani birden cok data geleceği için [] dizi şeklinde
+    // almamız gerekiyor
+    
+    func downloadCurrencies(url: URL, completion: @escaping ([CryptoCurrency]?)->(Void))  {
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            }
+            else if let data = data {
+                
+               let crytoList = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
+                
+                completion(crytoList)
+                
+                if let cryptoList = crytoList {
+                    completion(cryptoList)
+                }
+            }
+        }.resume() // işlemi başlatmamıza yarıyan 
+    }
+}
--- CryptoApp/View/ViewController.swift
+++ CryptoApp/View/ViewController.swift
@@ -16,6 +16,15 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
+        
+        let url = URL(string: "https://api.nomics.com/v1/currencies/sparkline?key=3176b6783f605328be697faceefa8833&ids=BTC,ETH,XRP&start=2021-04-10T00%3A00%3A00Z")!
+        
+        Webservice().downloadCurrencies(url:url) { (cryptos) in
+            if let cryptos = cryptos {
+               
+            }
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
